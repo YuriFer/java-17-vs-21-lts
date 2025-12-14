b690f62d8ab39965dafcf91ddab9c7ab4f1e3761
Yuri <yuri_fernandes2000@hotmail.com>
2025-12-14 19:13:22 -0300

    1 - SequencedCollections Java 21

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java-17-vs-21-lts
 Este repositório demonstra, de forma prática, as principais diferenças entre Java 17 e Java 21.
+
+## Principais temas
+- Virtual Threads
+- Structured Concurrency
+- Pattern Matching
+- Sequenced Collections
---
+++ SequencedCollections.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequencedCollections {
+
+    // Benefício se aplica para Collections, Maps e Sets
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>(){
+            {
+                add("Banana");
+                add("Uva");
+                add("Laranja");
+            }
+        };
+
+        // Antes - Java 17
+        String lastElementBefore = arrayList.get( arrayList.size() - 1 );
+        // Depois - Java 21
+        String lastElementAfter = arrayList.getLast();
+        List<String> reversedList = arrayList.reversed();
+
+        System.out.println("Último elemento java 17: " + lastElementBefore);
+        System.out.println("Último elemento java 21: " + lastElementAfter);
+
+        System.out.println(reversedList);
+        
+        /* Principais operações adicionadas: 
+            getFirst(), 
+            getLast(), 
+            reversed(), 
+            addFirst(E element), 
+            addLast(E element),
+            removeFirst(),
+            removeLast() 
+        */
+    }    
+}
